f58f7c5ecdb250160c36223e437c66a549c70815
Ali Zaidi <msazaidi@gmail.com>
2019-02-02 23:03:29 -0800

    added update and complete api endpoints

--- api.py
+++ api.py
@@ -13,37 +13,73 @@ app.config['MONGO_URI'] = 'mongodb://localhost:27017/Todo'
 mongo = PyMongo(app, uri = "mongodb://localhost:27017/TodoDB")
 
 # Get all todos
-@app.route('/api/v1/resources/new', methods=['POST'])
-def new():
-	input_json = request.get_json(force=True)
-
-	todo = mongo.db.todos
-	todo.insert({'id': input_json['id'], 'text': input_json['text'], 'complete': input_json['complete']})
-	return "Success"
-
-# Post Todo
 @app.route('/api/v1/resources/todos/all', methods=['GET'])
 def get_all_todos():
 	todos = mongo.db.todos
 	output = []
 	for todo in todos.find():
-		output.append({'id' : todo['id'], 'text' : todo['text'], 'complete' : todo['complete']})
+		output.append({'text' : todo['text'], 'complete' : todo['complete']})
 	return jsonify({'result' : output})
+
+# Post Todo
+@app.route('/api/v1/resources/new', methods=['POST'])
+def new():
+	input_json = request.get_json(force=True)
+
+	todo = mongo.db.todos
+	todo.insert({'text': input_json['text'], 'complete': input_json['complete']})
+	return "Success"
 
 # Get Todo by Id
 @app.route('/api/v1/resources/todos/<id>', methods=['GET'])
 def get_todo_by_id(id):
 	todos = mongo.db.todos
 	todo = todos.find_one({'_id':bson.ObjectId(oid=str(id))})
-	print todo
 
 	sanitized = json.loads(json_util.dumps(todo))
 	return jsonify(sanitized)
 
-# Update ID
-def update_todo():
-	id = request.values.get("_id")
-	todo = todos.find({"_id":ObjectId(id)})
-		
+@app.route("/api/v1/resources/todos/<id>", methods=['DELETE'])
+def remove_todo(id):
+	todos = mongo.db.todos
+	try:
+		delete_todo = todos.delete_one({'_id':bson.ObjectId(oid=str(id))})
+
+		if delete_todo.deleted_count > 0:
+			# Prepare the response
+			return "", 204
+		else:
+			# Resource not found
+			return "", 404
+	except:
+		# Error while trying to delete the resource
+		# Add message for debugging purpose
+		return "", 500
+
+@app.route('/api/v1/resources/todos/<id>', methods=['PUT'])
+def update_todo(id):
+	input_json = request.get_json(force=True)
+
+	todos = mongo.db.todos
+	todo = todos.update_one({'_id':bson.ObjectId(oid=str(id))}, {'$set': {'text': input_json['text']}})
+
+	return 'updated'
+
+@app.route('/api/v1/resources/todos/complete', methods=['GET'])
+def complete_todo():
+	the_id = request.args.get('id')
+
+	print the_id
+
+	todos = mongo.db.todos
+	todo = todos.find_one({'_id':bson.ObjectId(oid=str(the_id))})
+	if todo['complete'] == False:
+		todos.update_one({'_id':bson.ObjectId(oid=str(the_id))},{'$set': {'complete': True }})
+	else:
+		todos.update_one({'_id':bson.ObjectId(oid=str(the_id))},{'$set': {'complete': False}})
+
+	return 'complete'
+
+
 
 app.run()
